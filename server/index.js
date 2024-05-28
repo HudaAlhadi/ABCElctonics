d96f1b46dc3b2e7ae98c5cb52d500d4004e910e7
Huda <alhadi.huda20@gmail.com>
2024-05-28 22:03:47 +0100

    F

--- server/index.js
+++ server/index.js
@@ -17,17 +17,29 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 // Initialize Express app
 const app = express();
 
-// Body parser middleware (included in Express)
-app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://abcelectonics-43364e8a49cb.herokuapp.com');
-  next();
-});
+// CORS configuration
+const allowedOrigins = ['https://abcelectonics-43364e8a49cb.herokuapp.com', 'http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, curl, postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
 // Use cookie parser middleware
 app.use(cookieParser());
 
-
-
+// Body parser middleware (included in Express)
+app.use(express.json());
 
 // MongoDB connection
 mongoose.connect('mongodb+srv://hudaalhadi:elc.eng18@ecommerce.a0l0yl8.mongodb.net/ecommerce', {
