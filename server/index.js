81d02688ad71d449a94eca1e0c62ad0f7e108d2a
Huda <alhadi.huda20@gmail.com>
2024-05-28 20:48:58 +0100

    h

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use(cors({
 
 // Enable CORS for all routes (Alternative method)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://abcelectonics-43364e8a49cb.herokuapp.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
