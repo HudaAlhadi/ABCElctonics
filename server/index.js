3098d412b8e1ffd0481d9000ee02b0c467da98d7
Huda <alhadi.huda20@gmail.com>
2024-05-29 22:58:32 +0100

    update index.js

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use('/user', route);
 app.use('/products', productRoute);
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
